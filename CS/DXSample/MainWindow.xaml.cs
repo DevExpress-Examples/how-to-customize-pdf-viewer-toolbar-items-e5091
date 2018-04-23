fd3e28dda93a65efb509755946ede2909a370b92
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXSample/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using DevExpress.Utils;
+using System.Reflection;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Mvvm;
+using System.Windows.Input;
+using DevExpress.Xpf.PdfViewer;
+using DevExpress.Xpf.WindowsUI;
+using System.Collections.Generic;
+
+namespace DXSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileStream pdfStream = new FileStream("Demo.pdf", FileMode.Open);
+            Viewer.DocumentSource = pdfStream;
+        }
+
+        private void bCustomItem_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            WinUIDialogWindow w = new WinUIDialogWindow();
+            w.Content = "Hello world!";
+            w.ShowDialogWindow(MessageBoxButton.YesNo);
+        }
+    }
+}
---
+++ VB/DXSample/MainWindow.xaml.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.IO
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Utils
+Imports System.Reflection
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Mvvm
+Imports System.Windows.Input
+Imports DevExpress.Xpf.PdfViewer
+Imports DevExpress.Xpf.WindowsUI
+Imports System.Collections.Generic
+
+Namespace DXSample
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits DXWindow
+		Public Sub New()
+			InitializeComponent()
+			Dim pdfStream As New FileStream("Demo.pdf", FileMode.Open)
+			Viewer.DocumentSource = pdfStream
+		End Sub
+
+		Private Sub bCustomItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+			Dim w As New WinUIDialogWindow()
+			w.Content = "Hello world!"
+			w.ShowDialogWindow(MessageBoxButton.YesNo)
+		End Sub
+	End Class
+End Namespace
