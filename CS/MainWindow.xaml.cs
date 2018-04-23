eba22bc124af1a9a35dffff4060756332044b998
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace PdfViewerCustomization {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
